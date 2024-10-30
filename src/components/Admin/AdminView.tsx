2b08ee6148c54b108e6e783f87ee8bd61c7b18fb
restrepoTorres <vmanuel.restrepo@udea.edu.co>
2024-10-29 21:25:27 -0500

    feat: basic admin view

--- src/components/Admin/AdminView.tsx
+++ src/components/Admin/AdminView.tsx
@@ -1,15 +1,17 @@
 import React from "react";
 import "react-modern-drawer/dist/index.css";
+import Edit from "./Edit";
 
 const AdminView = () => {
-  
-
   return (
-   <>
-   <button>Editar Materia</button>
-   <br />
-   <button>Crear Materia</button>
-   </>
+    <>
+      <button>Editar Materia</button>
+      <br />
+      <button>Crear Materia</button>
+      <Edit></Edit>
+      <a href="/login">holita</a>
+      
+    </>
   );
 };
 
